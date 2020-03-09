222a65c2042d52b34fa1a3f1a320f3d3f83a6e54
Joe Thompson <joseph.thompson>
2020-03-09 21:03:23 +0000

    added site to create a procedural test area to test the crawler on locally, this will simulate a real site that has content on it, and we can set it to take the time a real site might without using bandwidth

---
+++ FinalCrawler.ProceduralSite/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FinalCrawler.ProceduralSite.Models;
+
+namespace FinalCrawler.ProceduralSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("{*url}")]
+        public IActionResult Index()
+        {
+            // get info about what to do from url
+
+            // wait for some amount of milliseconds here to simulate a real server
+
+            // return a length of new links, emails, images, etc
+
+            // the view will display them for the crawler to find
+
+            return View();
+        }
+    }
+}
---
+++ FinalCrawler.ProceduralSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FinalCrawler.ProceduralSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
